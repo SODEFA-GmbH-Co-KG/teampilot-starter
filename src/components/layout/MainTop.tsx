90ef2f2579d7e896987c9253bd28bd6b2201afc5
Tristan Rechenberger <rechenberger@me.com>
2023-09-22 12:33:08 +0200

    🎉

--- src/components/layout/MainTop.tsx
+++ src/components/layout/MainTop.tsx
@@ -1,4 +1,6 @@
 import { DarkModeToggle } from '@/components/layout/DarkModeToggle'
+import { Button } from '@/components/ui/button'
+import { Github } from 'lucide-react'
 import Link from 'next/link'
 import { MainTopNav } from './MainTopNav'
 
@@ -9,7 +11,7 @@ export const MainTop = () => {
         <Link href="/" className="flex flex-row items-center gap-3">
           <div className="text-xl">
             <strong>
-              Teampilot <span className="text-primary">AI</span>
+              Teampilot <span className="text-primary">Starter</span>
             </strong>
           </div>
         </Link>
@@ -17,6 +19,14 @@ export const MainTop = () => {
           <MainTopNav />
         </div>
         <div className="flex flex-row">
+          <Link
+            href="https://github.com/SODEFA-GmbH-Co-KG/teampilot-starter"
+            target="_blank"
+          >
+            <Button variant={'ghost'} size="icon">
+              <Github />
+            </Button>
+          </Link>
           <DarkModeToggle />
         </div>
       </div>
